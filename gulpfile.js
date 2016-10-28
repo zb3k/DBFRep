52b61e5d4fc650b1fa74fde9f053efa2156a10ec
zb3k <d.bourim@gmail.com>
2016-10-29 01:23:39 +0300

    add some styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,25 @@
-var gulp = request('gulp');
+var gulp = require("gulp");
+var koutoSwiss = require("kouto-swiss");
+var stylus = require("gulp-stylus");
+var livereload = require('gulp-livereload');
+
+
+var isProduction = false;
+var buildTasks = ['styles'];
+
+
+gulp.task('styles', function(){
+    gulp.src('src/styles/app.styl')
+        .pipe(stylus({
+            use: koutoSwiss(),
+            compress: isProduction
+          }))
+        .pipe(gulp.dest("src/styles/"))
+        .pipe(livereload());
+});
+
+
+gulp.task('watch', buildTasks, function(){
+  livereload.listen();
+  gulp.watch('src/styles/*.styl', ['styles']);
+});
